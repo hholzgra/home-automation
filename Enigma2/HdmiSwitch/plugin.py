9b4edd81a8c713dedf04347067dbae71765eb46d
Hartmut Holzgraefe <hartmut@php.net>
2016-10-06 00:53:28 +0200

    * removed serial output * added plugin logo * added direct number key selection shortcuts

--- Enigma2/HdmiSwitch/plugin.py
+++ Enigma2/HdmiSwitch/plugin.py
@@ -1,7 +1,7 @@
 import os
 from Screens.Screen import Screen
 from Components.MenuList import MenuList
-from Components.ActionMap import ActionMap
+from Components.ActionMap import ActionMap, NumberActionMap
 from Screens.MessageBox import MessageBox
 from Plugins.Plugin import PluginDescriptor
 
@@ -9,62 +9,69 @@ from Plugins.Plugin import PluginDescriptor
 
 class HdmiSwitch(Screen):
     skin = """<screen position="100,150" size="460,400" title="HDMI Quellen" >
-                  <widget name="myMenu" position="10,10" size="420,380" scrollbarMode="showOnDemand" />
+                  <widget name="myMenu" position="10,10" size="420,380" scrollbarMode="showOnDemand" enableWrapAround="1" />
               </screen>"""
               
-    def sendCmd(self, cmd1, cmd2):
-        device1 = "/dev/ttyS0"
-        device1_options = "-hupcl 19200"
-       
-        os.system("stty %s -F %s" % (device1_options, device1))
-        f1 = open(device, "w")
-        f1.write(cmd1+"\n")
-        f1.close()
+    def sendCmd(self, cmd):
+        print "CMD '%s'" % cmd
+        device = "/dev/ttyUSB0"
+        device_options = "-hupcl 9600"
 
-        device2 = "/dev/ttyS0"
-        device2_options = "-hupcl 9600"
-
-        os.system("stty %s -F %s" % (device2_options, device2))
-        f2 = open(device, "w")
-        f2.write(" "+cmd2+"\n")
-        f2.close()
-
+        os.system("stty %s -F %s" % (device_options, device))
+        f = open(device, "w")
+        f.write(" ")
+        f.write(cmd)
+        f.write(cmd)
+        f.write(cmd)
+        f.write("\n")
+        f.close()
 
     def __init__(self, session, args =0):
         self.session = session
        
         # switch to first port, assuming that this is the Sat receiver port
         # to make sure the menu becomes visible
-        self.sendCmd("swi01o01", "A")
+        self.sendCmd("A")
         
         list = []
-        list.append(("SAT-Fernsehn",     "swi01o01", "A"));
-        list.append(("AppleTV",          "swi02o01", "B"));
-        list.append(("DVD-Player",       "swi03o01", "C"));
-        list.append(("Raspberry Pi",     "swi04o01", "D"));
-        list.append(("ChromeStick",      "swi05o01", "E"));
-        list.append(("-- frei --",       "swi06o01", "F"));
-        list.append(("Amazon FireStick", "swi07o01", "G"));
-        list.append(("Laptop",           "swi08o01", "H"));
+        list.append(("1 SAT-Fernsehn",     "A"));
+        list.append(("2 AppleTV",          "B"));
+        list.append(("3 DVD-Player",       "C"));
+        list.append(("4 Raspberry Pi",     "D"));
+        list.append(("5 ChromeStick",      "E"));
+        list.append(("6 -- frei --",       "F"));
+        list.append(("7 Amazon FireStick", "G"));
+        list.append(("8 Laptop-Kabel",     "H"));
         
         Screen.__init__(self, session)
         self["myMenu"] = MenuList(list)
-        self["myActionMap"] = ActionMap(["SetupActions"],
+        self["myActionMap"] = NumberActionMap(["SetupActions", "InputActions"],
                                         {
                                             "ok": self.go,
-                                            "cancel": self.cancel
+                                            "cancel": self.cancel,
+                                            "1": self.keyNumber,
+                                            "2": self.keyNumber,
+                                            "3": self.keyNumber,
+                                            "4": self.keyNumber,
+                                            "5": self.keyNumber,
+                                            "6": self.keyNumber,
+                                            "7": self.keyNumber,
+                                            "8": self.keyNumber,
                                         } , -1)
         
         
     def go(self):
-        retVal1 = self["myMenu"].l.getCurrentSelection()[1]
-        retVal2 = self["myMenu"].l.getCurrentSelection()[2]
+        selection = self["myMenu"].l.getCurrentSelection()[1]
 
-        if returnValue is not None:
-            self.sendCmd(retVal1, retVal2)
+        if selection is not None:
+            self.sendCmd(selection)
             
         self.close(None)
         
+    def keyNumber(self, number):
+	self.sendCmd(str(chr(number + ord('A') - 1)))
+        self.close(None)
+
     def cancel(self):
         self.close(None)
 
@@ -79,5 +86,6 @@ def Plugins(**kwargs):
     return PluginDescriptor(name="HDMI Switch",
                             description="ATEN VS0801H HDMI Switch RS232 Control",
                             where = PluginDescriptor.WHERE_PLUGINMENU,
+			    icon = "arduino-HDMI.png",
                             fnc=main)
 
